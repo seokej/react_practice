8313db87f5482a918ad196b9b2d8f4e00d00d0cc
sej <ejseog@gmail.com>
2026-02-01 18:53:56 +0900

    Update App.tsx
    에러 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import './App.css';
-import './reset.css';
-import Calc from './components/calculator/Calc';
-import Addition from './components/addition/Addition';
+import "./App.css";
+import "./reset.css";
+// import Calc from "./components/calculator/Calc";
+import Addition from "./components/addition/Addition";
 
 function App() {
-    return (
-        <>
-            {/* <Calc/> */}
-            <Addition/>
-        </>
-    ) 
+  return (
+    <>
+      {/* <Calc/> */}
+      <Addition />
+    </>
+  );
 }
 
 export default App;
